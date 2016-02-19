9a5bf4bfd56a9012e80d05798a70cb7dd5c4cebf
xieguoxing <xieguoxing@tftiancai.com>
2016-02-19 15:19:56 +0800

    完成不等比例按钮布局

---
+++ demo_Button/demo_Button/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo_Button
+//
+//  Created by LittleKin on 16/1/27.
+//  Copyright © 2016年 LittleKin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo_Button/demo_Button/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo_Button
+//
+//  Created by LittleKin on 16/1/27.
+//  Copyright © 2016年 LittleKin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
